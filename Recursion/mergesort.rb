a7bf80ed3fe7d0f605e63eec219e698e4504bae2
denisshapiro <denis.o.shapiro@gmail.com>
2019-07-16 19:08:00 -0500

    finished recursive mergesort

--- Recursion/mergesort.rb
+++ Recursion/mergesort.rb
@@ -1,5 +1,32 @@
 def merge_sort(arr)
-    arr.sort
+  if arr.length <= 1
+    arr
+  else
+    left = merge_sort(arr[0...arr.size/2])
+    right = merge_sort(arr[arr.size/2..arr.size])
+    merge(left, right)
+  end
 end
 
-p merge_sort([2, 6, 1, 4, 0, 5, 3])
+def merge(l1, l2, arr = [])
+  if l1.empty? && l2.empty?
+    arr
+  elsif l1.empty? && !l2.empty?
+    arr + l2
+  elsif !l1.empty? && l2.empty?
+    arr + l1
+  elsif l1.first <= l2.first
+    arr << l1.shift
+    merge(l1, l2, arr)
+  elsif l1.first >= l2.first
+    arr << l2.shift
+    merge(l1, l2, arr)
+  end
+end
+
+p merge_sort([2, 3, 6, 12, 4, 45, 1])
+p merge_sort([-21, 10, -44, 99, 0])
+p merge_sort([5, 1, 3, 4, 2])
+
+rand_arr = 1000.times.map{Random.rand(-1_000_000..1_000_000) }
+p merge_sort(rand_arr)
